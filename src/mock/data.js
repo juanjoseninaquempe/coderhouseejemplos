caa43c00acd765304c9284d1938748e55627d1da
JUAN jose <jnina@frba.utn.edu.ar>
2023-08-29 15:06:38 -0300

    pre-entrega2

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,7 +1,7 @@
 const list = [
     {
         id: "1",
-        category: "nuevos",
+        category: "ofertas",
         name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
         price: 109.95,
         description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
@@ -10,7 +10,7 @@ const list = [
     },
     {
         id: "2",
-        category: "ofertas",
+        category: "vendidos",
         name: "Mens Casual Premium Slim Fit T-Shirts ",
         price: 22.3,
         description: "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
